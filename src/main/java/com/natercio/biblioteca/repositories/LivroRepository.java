57b6de4fd3d7e918612fc6c8517e258d82c50eff
ncnogueira <ncnogueira@gmail.com>
2022-08-18 15:49:42 -0300

    repositorios implementados

---
+++ src/main/java/com/natercio/biblioteca/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.natercio.biblioteca.repositories;
+
+import com.natercio.biblioteca.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
