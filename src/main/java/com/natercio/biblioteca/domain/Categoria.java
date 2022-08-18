52206c4b02be2dd4b397b10fd92d448e16aac51f
ncnogueira <ncnogueira@gmail.com>
2022-08-18 15:38:23 -0300

    finalizando as classes de domínio

--- src/main/java/com/natercio/biblioteca/domain/Categoria.java
+++ src/main/java/com/natercio/biblioteca/domain/Categoria.java
@@ -1,19 +1,27 @@
 package com.natercio.biblioteca.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+@Entity
+public class Categoria implements Serializable {
 
-public class Categoria {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     public Categoria() {
         super();
     }
+
 
     public Categoria(Integer id, String nome, String descricao) {
         this.id = id;
