df14c064e76c14be10a9bbeb1ae16e48b1b57880
ncnogueira <ncnogueira@gmail.com>
2022-08-29 13:26:27 -0300

    Perfil de Dev criado novamente

--- src/main/java/com/natercio/biblioteca/config/DevConfig.java
+++ src/main/java/com/natercio/biblioteca/config/DevConfig.java
@@ -1,11 +1,12 @@
 package com.natercio.biblioteca.config;
 
-import com.natercio.biblioteca.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.natercio.biblioteca.service.DBService;
 
 @Configuration
 @Profile("dev")
