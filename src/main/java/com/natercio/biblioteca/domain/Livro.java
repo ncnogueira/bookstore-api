7fc1ca203b5b05716201a748fcd608141d2f089a
ncnogueira <ncnogueira@gmail.com>
2022-08-29 13:53:26 -0300

    findById() para Categoria

--- src/main/java/com/natercio/biblioteca/domain/Livro.java
+++ src/main/java/com/natercio/biblioteca/domain/Livro.java
@@ -1,5 +1,7 @@
 package com.natercio.biblioteca.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
@@ -15,6 +17,7 @@ public class Livro implements Serializable {
     private String nome_autor;
     private String texto;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
---
+++ src/main/java/com/natercio/biblioteca/service/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.natercio.biblioteca.service;
+
+import com.natercio.biblioteca.domain.Categoria;
+import com.natercio.biblioteca.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    public Categoria findById(Integer id){
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
